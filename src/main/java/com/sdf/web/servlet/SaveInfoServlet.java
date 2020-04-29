ee01afd308511cfd7218a03db5274c7986a7a9a6
antonyyu0119 <784259092@qq.com>
2020-04-29 19:31:25 +0800

    登录、注册、修改信息Servlet

---
+++ src/main/java/com/sdf/web/servlet/SaveInfoServlet.java
@@ -0,0 +1,73 @@
+package com.sdf.web.servlet;
+
+import com.sdf.domain.ResultInfo;
+import com.sdf.domain.User;
+import com.sdf.service.UserService;
+import com.sdf.service.impl.UserServiceImpl;
+import com.sdf.utils.MsgHouseUtils;
+import com.sdf.utils.ServletUtils;
+import org.apache.commons.beanutils.BeanUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+@WebServlet("/saveInfoServlet")
+public class SaveInfoServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=utf-8");
+
+        //创建业务层对象
+        UserService userService = new UserServiceImpl();
+        //创建结果集对象
+        ResultInfo res = null;
+        //创建响应数据
+        String json = null;
+        //创建用户对象
+        User user = new User();
+        //获取请求参数
+        Map<String, String[]> map = request.getParameterMap();
+        try {
+            BeanUtils.populate(user,map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        //修改
+        if(userService.changeUserInfo(user)){
+            res = ServletUtils.getInfo(true,user,"修改成功");
+            json = ServletUtils.getJsonInfo(res);
+            response.getWriter().write(json);
+        }else{
+            res = ServletUtils.getInfo(false,null, MsgHouseUtils.changeErrorMsg);
+            json = ServletUtils.getJsonInfo(res);
+            response.getWriter().write(json);
+        }
+
+        /*String action = request.getParameter("action");
+        if("query".equals(action)){
+            //userDao.queryInfo();
+            return;
+        }*/
+        //System.out.println("name-->" + user.getName() + "age--->" + user.getAge() + "gender--->" + user.getGender() + "address--->" + user.getAddress());
+
+        //userDao.updateInfo(user);
+
+        /*ServletOutputStream sos = response.getOutputStream();
+        sos.write("success".getBytes("utf-8"));*/
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        this.doPost(request,response);
+    }
+}
---
+++ src/main/java/com/sdf/web/servlet/UserRegistServlet.java
@@ -0,0 +1,61 @@
+package com.sdf.web.servlet;
+
+import com.sdf.domain.ResultInfo;
+import com.sdf.domain.User;
+import com.sdf.service.UserService;
+import com.sdf.service.impl.UserServiceImpl;
+import com.sdf.utils.MsgHouseUtils;
+import com.sdf.utils.ServletUtils;
+import org.apache.commons.beanutils.BeanUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+@WebServlet(name = "UserRegistServlet")
+public class UserRegistServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=utf-8");
+
+        //创建业务层对象
+        UserService userService = new UserServiceImpl();
+        //创建结果集对象
+        ResultInfo res = null;
+        //创建响应数据
+        String json = null;
+        //创建用户对象
+        User registUser = new User();
+        //获取请求参数
+        Map<String, String[]> map = request.getParameterMap();
+
+        try {
+            BeanUtils.populate(registUser,map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        //注册
+        if(userService.UserRegist(registUser)){
+            res = ServletUtils.getInfo(true,null,"注册成功");
+            json = ServletUtils.getJsonInfo(res);
+            response.getWriter().write(json);
+        }else{
+            res = ServletUtils.getInfo(false,null, MsgHouseUtils.registerErrorMsg);
+            json = ServletUtils.getJsonInfo(res);
+            response.getWriter().write(json);
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        this.doPost(request,response);
+    }
+}
